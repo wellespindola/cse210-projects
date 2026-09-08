b38b8b030d6875f3fe34032c2c7e2def7fc835ae
Wellington Espindola <espindola.well@gmail.com>
2026-09-08 21:36:50 +0100

    Complete W02 Resume abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;   
+        
+      
+
+        Job job2 = new Job();
+        job2. _company = "Google";
+        job2._jobTitle = "Ceo";
+        job2._startYear = 2014;
+        job2._endYear = 2025;
+        
+        
+
+        Resume myResume = new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._name = "wellington";
+        myResume.Display();
+        
     }
 }
